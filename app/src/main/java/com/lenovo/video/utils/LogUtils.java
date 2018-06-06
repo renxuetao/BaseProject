709bebe56c6372747cd7a48d5a1bbee3a0e1ea7f
renxt1 <renxt1@lenovo.com>
2018-06-06 23:56:13 +0800

    fix bug

--- app/src/main/java/com/lenovo/video/utils/LogUtils.java
+++ app/src/main/java/com/lenovo/video/utils/LogUtils.java
@@ -6,35 +6,47 @@ import com.orhanobut.logger.Logger;
 
 public class LogUtils {
 
-    private static final String TAG = "LauncherVideo=====";
+    private static final String TAG = "VideoPhone:";
 
     public static void e(String tag, Object message) {
-        if (Constants.isLog)
-            Logger.e(TAG + tag, message + "");
+        if (Constants.isLog) {
+//            Logger.e(TAG + tag, message + "");
+            Logger.e(tag + ":" + message + "");
+        }
     }
 
     public static void e(Object message) {
-        if (Constants.isLog)
-            Logger.e(TAG, message + "");
+        if (Constants.isLog) {
+            Logger.e(message + "");
+//        Logger.e(TAG, message + "");
+        }
     }
 
     public static void d(String tag, Object message) {
-        if (Constants.isLog)
-            Logger.d(TAG + tag, message + "");
+        if (Constants.isLog) {
+            Logger.d(tag + ":" + message + "");
+//        Logger.d(TAG + tag, message + "");
+        }
     }
 
     public static void d(Object message) {
-        if (Constants.isLog)
-            Logger.d(TAG, message + "");
+        if (Constants.isLog) {
+            Logger.d(message + "");
+//        Logger.d(TAG, message + "");
+        }
     }
 
     public static void i(String tag, Object message) {
-        if (Constants.isLog)
-            Logger.i(TAG + tag, message + "");
+        if (Constants.isLog) {
+            Logger.i(tag + ":" + message + "");
+//        Logger.i(TAG + tag, message + "");
+        }
     }
 
     public static void i(Object message) {
-        if (Constants.isLog)
-            Logger.i(TAG, message + "");
+        if (Constants.isLog) {
+            Logger.i(message + "");
+//        Logger.i(TAG, message + "");
+        }
     }
 }
